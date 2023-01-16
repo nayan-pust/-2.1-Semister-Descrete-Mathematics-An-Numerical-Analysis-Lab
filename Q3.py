89a0c2ffa80c1325e9d46f6a4694cc1b4655549a
nayan-pust <114688354+nayan-pust@users.noreply.github.com>
2023-01-16 22:47:05 +0600

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -19,3 +19,19 @@ print(color_nodes(graph))
 
 #Output: 
 #{'c': 0, 'a': 1,'d': 2, 'e': 1, 'b': 2, 'f': 2}
+
+Procedure: Firstly, I created a user define functions called color_nodes for calculating graph 
+coloring by Welch-Powell’s algorithm and should be used ‘def’ for definition keyword before 
+function name for created this. This function contained one parameter ‘graph’.
+After that, I created an empty list and assigned with a variable named color_map under the 
+function. Again, I created a ‘for’ loop for consider nodes in descending degree (most neighbors 
+into least neighbors) using sorted function that’s received three parameters such as iterable 
+objects(list), lambda function, reverse method. Here iterable object is graph, ‘lambda’ is an 
+anonymous function that calculated length of graph using len() function. Inside this ‘for’ loop I 
+used a comprehension list for calculated neighbor colors and assigned the appropriate result with 
+a variable named neighbor_colors as a set. Then I also used a next function. Inside the next function 
+I created a comprehension list which contained an ‘if’ statement that checked the condition “color 
+not in neighbor_colors” and assigned the appropriate result with a variable named color_map. 
+Finally, I returned the result of color_map.
+After that, I created a dictionary for stored the adjacent list and assigned this dictionary with a 
+variable named graph. Dictionary are used stored the value with key of pair
