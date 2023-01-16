e15bebac1ed7456a5918c6117c9a90ac0c3a6c6d
nayan-pust <114688354+nayan-pust@users.noreply.github.com>
2023-01-16 22:55:10 +0600

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -33,6 +33,27 @@ print(data)
 #Enter Second file name with extension: data5.txt
 #R = [(2, 1), (3, 1), (3, 2)]
 #Matrix, R = 
+
+Procedure: This program start with “import numpy as np”. NumPy is a Python library used for 
+working with arrays. In Python we have lists that serve the purpose of arrays, but they are slow to 
+process. NumPy aims to provide an array object that is up to 50x faster than traditional Python 
+lists. The means of “import numpy as np” is Now the NumPy package can be referred to 
+as np instead of numpy.
+Firstly, I created two files data4.txt file and data5.txt file in the same folder and here putted values 
+of the sets for getting input. Now, for getting input from data4.txt file, I used input function for 
+getting input and assigned the first file with a variable named file_name1. After that I used open 
+method and also used file mode “r” for reading the file and putted the first file with a variable 
+named file1. Similarly, I putted the second file name with a difference variable named file_name2 
+and also assigned the second file with a difference variable named file2.
+After that, I used map function. It has received two parameters, such as function and iterable object, 
+it returns a list and I putted the list with a variable named list1. I used the same things again, but 
+this time I putted the student.txt file with a different variable named file2 and also putted the list 
+with a different variable named list2.
+Afterwards, I used list comprehension for calculating some specific mathematical tasks. First task 
+is putted with a variable named output and second task is putted with a variable named output2.
+Then I used an attribute from numpy named np.array and putted the result with a variable named 
+arr. After that, I used reshaping array that means changing the shape of an array. I used 
+arr.reshape(3,2) and putted the result with a variable named newarr. Lastly, I printed the result.
 # [0 0]
  #[1 0]
  #[1 1]
