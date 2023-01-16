04a4560c4142581ae7c8e7d25d4d642362a85622
nayan-pust <114688354+nayan-pust@users.noreply.github.com>
2023-01-16 22:50:02 +0600

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -61,3 +61,22 @@ print(sum)
 
 #  value at !946 :32.34375
 
+Procedure: This program started with a user defined function named u_cal. It has received two 
+parameters, u and n. Under this function, firstly I created a variable named temp and assign the 
+value of u. Then I used ‘for’ loop, under this loop I done some specific task and putted the result 
+in temp variable. Lastly, I returned the temp variable.
+After that, I again used another user defined function named “fact” for calculating factorial of 
+given number n. 
+Afterwards, I declared n variable and assigned the value 6. Then I declared an array named x. After 
+that, I used comprehension list and putted the result in variable y.
+Subsequently, I assigned y[0][0]=12; y[1][0]=15; y[2][0]=20; y[3][0]=27; y[4][0]=39; y[5][0]=52.
+For calculating the forward difference table I used two ‘for’ loop in nesting style. Then I again 
+used two for loop in nesting style for displaying the forward difference table.
+After that, I created a variable named ‘value’ and assigned the interpolated value 1946. Then I 
+created another variable named sum and initializing sum = y[0][0]. Again, I created variable named 
+‘u’ and calculated some specific task and putted the result in variable ‘u’.
+Afterwards, I used for loop and calculated some specific tasks and putted the final result in variable 
+some. Here, I called two user defined u_cal and fact function. 
+Lastly, I printed the sum variable, here I used round function. The round () function returns a 
+floating point number that is a rounded version of the specified number, with the specified number 
+of decimals
